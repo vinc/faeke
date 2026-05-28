352b99b1e8d0344d2d421b9962dc5f82d9bd4de1
Vincent Ollivier <v.ollivier@wizville.fr>
2026-05-28 23:52:47 +0200

    Hide stderr

--- tts.sh
+++ tts.sh
@@ -1,3 +1,4 @@
 # pip install piper-tts
 # python -m piper.download_voices sw_CD-lanfrica-medium
-ruby faeke.rb espeak | tee /dev/tty | piper -m sw_CD-lanfrica-medium --length-scale 1.5
+ruby faeke.rb espeak | tee /dev/tty | \
+  piper --length-scale 1.5 --model sw_CD-lanfrica-medium 2>/dev/null
