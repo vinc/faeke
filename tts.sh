e745780d08c38518136c096503d12e35eaaf1b17
Vincent Ollivier <v.ollivier@wizville.fr>
2026-05-28 23:40:24 +0200

    Reduce TTS speed

--- tts.sh
+++ tts.sh
@@ -1,3 +1,3 @@
 # pip install piper-tts
 # python -m piper.download_voices sw_CD-lanfrica-medium
-ruby faeke.rb espeak | tee /dev/tty | piper -m sw_CD-lanfrica-medium
+ruby faeke.rb espeak | tee /dev/tty | piper -m sw_CD-lanfrica-medium --length-scale 1.5
