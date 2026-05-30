c53964e082ef24517215f6187639f14f756c3cfb
Vincent Ollivier <v@vinc.cc>
2026-05-30 22:33:01 +0200

    Update script

--- tts.sh
+++ tts.sh
@@ -1,4 +1,8 @@
 # pip install piper-tts
 # python -m piper.download_voices sw_CD-lanfrica-medium
+# paru -Ss piper-tts piper-voices-sw-cd
+
+data="/usr/share/piper-voices/sw/sw_CD/lanfrica/medium/"
+model="sw_CD-lanfrica-medium"
 ruby faeke.rb espeak | tee /dev/tty | \
-  piper --length-scale 1.5 --model sw_CD-lanfrica-medium 2>/dev/null
+  piper-tts --length-scale 1.5 --model "$model" --data-dir "$data" 2>/dev/null
